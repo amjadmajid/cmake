3e8b67d145632831fcebc5b9d19235801e887377
amjadmajid <amjad.y.majid@gmail.com>
2018-03-17 22:44:54 +0100

    link library to an executable

---
+++ basics_03_library/build/CMakeFiles/mathFuncs.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "libmathFuncs.a"
+)
---
+++ basics_04_library_linking/src/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "mathFuncs.h"
+
+int main()
+{
+    int x = add2Nums(3,4);
+    printf("The result of adding 3 and 4 is %d\n", x);
+    return 0;
+}
